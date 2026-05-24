5016f246e643395518be79efa4434325a5e50c69
Giang72147 <tgiang1360@gmail.com>
2026-05-25 00:25:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,89 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
 const app = express();
 
-const VERIFY_TOKEN = "vpkill";
+app.use(bodyParser.json());
 
-app.use(express.json());
+const PAGE_ACCESS_TOKEN = "DÁN_PAGE_TOKEN_VÔ_ĐÂY";
 
 app.get("/", (req, res) => {
-  res.send("Bot online");
+  res.send("VPKILL BOT ONLINE");
 });
 
-app.get("/webhook", (req, res) => {
-  const mode = req.query["hub.mode"];
-  const token = req.query["hub.verify_token"];
-  const challenge = req.query["hub.challenge"];
+app.post("/webhook", async (req, res) => {
 
-  if (mode && token === VERIFY_TOKEN) {
-    res.status(200).send(challenge);
+  const body = req.body;
+
+  if (body.object === "page") {
+
+    for (const entry of body.entry) {
+
+      const webhook_event = entry.messaging[0];
+
+      // USER NHẮN TIN
+      if (webhook_event.message) {
+
+        const sender_psid = webhook_event.sender.id;
+
+        // reply text
+        await callSendAPI(sender_psid, {
+          text: "💜 Chào mừng bạn đến với box VPKILL"
+        });
+      }
+
+      // USER VÀO BOX
+      if (webhook_event.member_joined) {
+
+        const sender_psid = webhook_event.sender.id;
+
+        await callSendAPI(sender_psid, {
+          text: "✨ Chào mừng thành viên mới đến với VPKILL"
+        });
+      }
+    }
+
+    res.status(200).send("EVENT_RECEIVED");
+
   } else {
-    res.sendStatus(403);
+    res.sendStatus(404);
   }
 });
 
-app.post("/webhook", (req, res) => {
-  console.log(req.body);
-  res.sendStatus(200);
+app.get("/webhook", (req, res) => {
+
+  const VERIFY_TOKEN = "vpkill_verify";
+
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  if (mode && token === VERIFY_TOKEN) {
+
+    if (token === VERIFY_TOKEN) {
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  }
 });
 
-const PORT = process.env.PORT || 3000;
+async function callSendAPI(sender_psid, response) {
+
+  await axios.post(
+    `https://graph.facebook.com/v19.0/me/messages?access_token=${PAGE_ACCESS_TOKEN}`,
+    {
+      recipient: {
+        id: sender_psid
+      },
+      message: response
+    }
+  );
+}
+
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
-  console.log("BOT RUNNING");
+  console.log(`BOT RUNNING ${PORT}`);
 });
